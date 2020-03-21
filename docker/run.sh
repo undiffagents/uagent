8c5f3696fbad0dba6af8ca96b8d5ce9fa5d9fa74
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-03-21 06:20:39 -0400

    Moved main run file to top level and docker files to their directory

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 eval $(docker-machine env uagent-local)
 
 echo "Building container..."
-docker build -t uagent .
+docker build -t uagent -f docker/Dockerfile .
 
 echo "Running container..."
 docker run -it uagent
