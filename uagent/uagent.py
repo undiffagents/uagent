5c1b1e408841b9807eb37392efcbb18d47accfcb
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-03-21 11:09:44 -0400

    Improved uagent

--- uagent/uagent.py
+++ uagent/uagent.py
@@ -9,8 +9,8 @@ from think import (Agent, Audition, Aural, Chunk, Hands, Item, Language,
 class UndifferentiatedAgent(Agent):
 
     def __init__(self, machine, output=True):
-        """Initializes the agent"""
-        super().__init__(output=True)
+        super().__init__(output=output)
+
         self.vision = Vision(self, machine.display)
         self.audition = Audition(self)
         self.hands = Hands(self)
@@ -21,68 +21,26 @@ class UndifferentiatedAgent(Agent):
         self.interpreter = Interpreter(self.ontology)
 
         self.language = Language(self)
-        self.language.add_interpreter(
-            lambda ace: self.interpreter.interpret_ace(ace))
+        self.language.add_interpreter(self.interpreter.interpret_ace)
 
-        # self.instruction = Instruction(
-        #     self, self.memory, self.audition, self.language)
-        # self.instruction.add_executor(self.execute)
+    # subject(subject), screen(screen), letter(target),
+    # hasProperty(present,positive), hasProperty(target,correct),
+    # appearsOn(target,screen), button(present)
+    # =>
+    # press(subject,present)
 
-    # def _interpret_predicate(self, text, isa='fact', last=None):
-    #     chunk = None
-    #     (pred, args) = text.replace(')', '').split('(')
-    #     args = args.split(',')
-    #     if len(args) == 1:
-    #         chunk = Chunk(isa=isa, predicate='isa',
-    #                       subject=args[0], object=pred)
-    #     elif len(args) == 2:
-    #         chunk = Chunk(isa=isa, predicate=pred,
-    #                       subject=args[0], object=args[1])
-    #     if chunk:
-    #         if last:
-    #             chunk.set('last', last.id)
-    #         self.memory.store(chunk)
-    #     return chunk
+    # button(absent), hasProperty(absent,negative),
+    # letter(distractor), screen(screen),
+    # appearsOn(distractor,screen), letter(distractor),
+    # hasProperty(distractor,incorrect), subject(subject)
+    # =>
+    # press(subject,absent)
 
-    # def _interpret_rule(self, text):
-    #     lhs, rhs = text.split('=>')
-    #     pred_pat = re.compile(r'[A-Za-z_-]+\([A-Za-z_,-]*\)')
-
-    #     rule = Chunk(isa='rule')
-    #     self.memory.store(rule)
-
-    #     last = rule
-    #     for t in pred_pat.findall(lhs):
-    #         chunk = self._interpret_predicate(t, isa='condition', last=last)
-    #         last = chunk
-
-    #     last = rule
-    #     for t in pred_pat.findall(rhs):
-    #         chunk = self._interpret_predicate(t, isa='action', last=last)
-    #         last = chunk
-
-    #     return rule
-
-    # def _interpret_drs(self, text):
-    #     text = text.replace(' ', '')
-    #     if text.find('=>') >= 0:
-    #         return self._interpret_rule(text)
-    #     else:
-    #         return self._interpret_predicate(text)
-
-    # def interpret(self, words):
-    #     return self._interpret_drs(''.join(words))
-
-    # def _deep_find(self, isa):
-    #     visual = self.vision.find(isa=isa, seen=False)
-    #     if visual:
-    #         return visual
-    #     else:
-    #         part_of = self.memory.recall(predicate='isPartOf', object=isa)
-    #         if part_of:
-    #             return self._deep_find(part_of.subject)
-    #         else:
-    #             return None
+    def is_action(self, rule):
+        for fact in rule.rhs:
+            if fact.pred == 'press':
+                return True
+        return False
 
     # def _execute_condition(self, cond, context):
     #     if cond.predicate == 'appearsIn':
@@ -121,12 +79,6 @@ class UndifferentiatedAgent(Agent):
 
     #         return True
 
-    def has_action(self, rule):
-        for fact in rule.rhs:
-            if fact.pred == 'press':
-                return True
-        return False
-
     def run(self, time=60):
 
         instr_visual = self.vision.wait_for(isa='instructions')
@@ -135,6 +87,6 @@ class UndifferentiatedAgent(Agent):
 
         while self.time() < time:
             for rule in self.ontology.get_ground_rules():
-                if self.has_action(rule):
+                if self.is_action(rule):
                     print(rule)
                 self.wait(1.0)
