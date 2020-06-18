bfb7656eeba4002139bc87ab83d5459654bdccbf
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-06-18 16:17:51 -0400

    Added docker volumes

--- docker/run.sh
+++ docker/run.sh
@@ -14,6 +14,9 @@ docker build \
 echo "Running container..."
 docker run \
     -it \
+    -v uagent-data:/usr/src/uagent/data \
+    -v uagent-run:/usr/src/uagent/run \
+    -v uagent-webapp:/usr/src/uagent/webapp \
     -p 4321:4321 \
     uagent
 
