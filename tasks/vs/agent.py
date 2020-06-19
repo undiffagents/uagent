a0e3d62ab369014d46f7186c5c2ea1def5fc594b
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-06-19 18:32:24 -0400

    Removed unused vs code

--- tasks/vs/agent.py
+++ tasks/vs/agent.py
@@ -9,24 +9,6 @@ class VSAgent(Agent):
         self.vision = Vision(self, env.display)
         self.motor = Motor(self, self.vision, env)
 
-    # def run(self, time):
-    #     while self.time() < time:
-    #         visual = self.vision.wait_for(
-    #             isa='letter', color='red', region='vs', seen=False)
-    #         obj = self.vision.encode(visual) if visual else None
-    #         while visual and obj != 'X':
-    #             visual = self.vision.find(
-    #                 isa='letter', color='red', region='vs', seen=False)
-    #             obj = self.vision.encode(visual) if visual else None
-    #         if obj == 'X':
-    #             # self.log('target present')
-    #             self.motor.type('w')
-    #             self.wait(1.0)
-    #         else:
-    #             # self.log('target absent')
-    #             self.motor.type('r')
-    #             self.wait(1.0)
-
     def run(self, time):
         while self.time() < time:
             self.vision.wait_for(isa='letter', seen=False)
