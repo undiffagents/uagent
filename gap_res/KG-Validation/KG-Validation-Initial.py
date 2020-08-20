db9c93661019cafceb0294366c611accc42310ae
Daniel Schmidt <schmidt.152@wright.edu>
2020-08-20 11:37:04 -0400

    Transitioned KG Validation to use OwlReady2
    Much cleaner and better now.  Going to try to change the instantiation system to OwlReady as well, making the whole process much better

--- gap_res/KG-Validation/KG-Validation-Initial.py
+++ gap_res/KG-Validation/KG-Validation-Initial.py
@@ -5,6 +5,8 @@ from owlready2 import *
 
 from ontoConstants import *
 
+instanceIndicator = 'Instance'
+
 # Initially hardcoding the structure of some links in the ontology just to test this system out on Items/Actions/etc.
 # Some of these may not be quite 'necessary' - e.g. Affordances for items.
 importantNodeSets = {ITEM_NODE: [ITEM_ROLE_NODE, AFFORDANCE_NODE, ITEM_DESCRIPTION_NODE],
@@ -13,43 +15,95 @@ importantNodeSets = {ITEM_NODE: [ITEM_ROLE_NODE, AFFORDANCE_NODE, ITEM_DESCRIPTI
 
 
 def checkInitializedGraph():
-    testGraph = networkx.MultiDiGraph()
-    testImport(testGraph)
-    for nodeType in importantNodeSets:
-        print("Expected nodes to be connected to " + nodeType + " nodes:")
-        print(importantNodeSets.get(nodeType))
+    onto = get_ontology("http://localhost:3030/uagent").load()
+    #testGraph = networkx.MultiDiGraph()
+    #testImport(testGraph)
+    for currentType in importantNodeSets:
         print('\n')
-    print('\n')
-    # Iterate through all nodes.  If a key name is found, get the node "n"'s predecessors and successors.
-    # Predecessor = directed edge from predecessor m to n, successor = directed edge from n to successor m.
-    # Check that all of the nodes in the value array exist as pred/succ.
-    for node in testGraph:
-        # This is messy and not great but whatever for now, it's proof-of-concept
-        for nodeSymbol in importantNodeSets.keys():
-            # If found something from one of the keys
-            nodeOfSetTypeFound = re.search(r'^' + nodeSymbol + r'\d+', node)
-            if nodeOfSetTypeFound:
-                # Get the list of successors and predecessors and check that all of the value array nodes are present
-                preds = list(testGraph.predecessors(node))
-                succs = list(testGraph.successors(node))
-                neighborNodes = preds + succs
-                # Default state is true - if one of the expected nodes is not found, then a flag is raised
-                nodeIsConnected = True
+        print("Expected nodes to be connected to " + currentType + " nodes:")
+        print(importantNodeSets.get(currentType))
+        print('\n')
+        with onto:
+            # Print list of instances of this item type
+            print(onto[currentType].instances())
+            # Iterate through all instances of the currently selected type
+            for currentInstance in onto[currentType].instances():
+                nodeFullyConnected = True
                 listOfMissingNodes = []
-                for expectedNode in importantNodeSets.get(nodeSymbol):
-                    # If the expected node is not found as a neighbord to the current node
-                    foundConnectedNode = any(re.search(r'^' + expectedNode + r'\d+', nNode) for nNode in neighborNodes)
-                    # Then mark that the current node is not fully connected and track which nodes are missing
+                for expectedNode in importantNodeSets.get(currentType):
+                    # Check if the expected node has a relationship to the current instance
+                    foundConnectedNode = checkForRelation(currentInstance, expectedNode)
+                    # If the expected node is note found, then flag the node as not fully connected and track missing
+                    # nodes
                     if foundConnectedNode == False:
-                        nodeIsConnected = False
+                        nodeFullyConnected = False
                         listOfMissingNodes.append(expectedNode)
                 # If the node is fully connected, it's good
                 # If the node is not fully connected, then flag it to the user and inform them of what's missing
-                if nodeIsConnected == True:
-                    print(node + " fully connected.")
+                if nodeFullyConnected == True:
+                    print(str(currentInstance) + " fully connected.")
                 else:
-                    print("WARNING: " + node + " not fully connected.  Expected nodes not found: "
-                          + str(listOfMissingNodes))
+                    print("WARNING: " + str(currentInstance) + " not fully connected.  Expected nodes not found: "
+                                        + str(listOfMissingNodes))
+
+    print('\n')
+
+def checkForRelation(currentInstance, expectedNode):
+    # Get all of the outgoing properties for the instance
+    for prop in currentInstance.get_properties():
+        # Get the target of the outgoing property
+        for propValue in prop[currentInstance]:
+            # Check if the property contains the expected node as the target
+            nodeFound = re.search(instanceIndicator + expectedNode + r'\d+', str(propValue))
+            # If so, return true and print the property
+            if nodeFound:
+                #print(currentInstance, prop.python_name, propValue)
+                return True
+    # Get all of the incoming properties for the instance
+    for inverseProp in currentInstance.get_inverse_properties():
+        # Get the source and value of the property
+        source, value = inverseProp
+        # Check if the property contains the expected node as the source
+        nodeFound = re.search(instanceIndicator + expectedNode + r'\d+', str(source))
+        # If so, return true and print the property
+        if nodeFound:
+            #print(source, value, currentInstance)
+            return True
+    # If nothing found, return False
+    return False
+
+
+    # Iterate through all nodes.  If a key name is found, get the node "n"'s predecessors and successors.
+    # Predecessor = directed edge from predecessor m to n, successor = directed edge from n to successor m.
+    # Check that all of the nodes in the value array exist as pred/succ.
+
+    # for node in testGraph:
+    #     # This is messy and not great but whatever for now, it's proof-of-concept
+    #     for nodeSymbol in importantNodeSets.keys():
+    #         # If found something from one of the keys
+    #         nodeOfSetTypeFound = re.search(r'^' + nodeSymbol + r'\d+', node)
+    #         if nodeOfSetTypeFound:
+    #             # Get the list of successors and predecessors and check that all of the value array nodes are present
+    #             preds = list(testGraph.predecessors(node))
+    #             succs = list(testGraph.successors(node))
+    #             neighborNodes = preds + succs
+    #             # Default state is true - if one of the expected nodes is not found, then a flag is raised
+    #             nodeIsConnected = True
+    #             listOfMissingNodes = []
+    #             for expectedNode in importantNodeSets.get(nodeSymbol):
+    #                 # If the expected node is not found as a neighbord to the current node
+    #                 foundConnectedNode = any(re.search(r'^' + expectedNode + r'\d+', nNode) for nNode in neighborNodes)
+    #                 # Then mark that the current node is not fully connected and track which nodes are missing
+    #                 if foundConnectedNode == False:
+    #                     nodeIsConnected = False
+    #                     listOfMissingNodes.append(expectedNode)
+    #             # If the node is fully connected, it's good
+    #             # If the node is not fully connected, then flag it to the user and inform them of what's missing
+    #             if nodeIsConnected == True:
+    #                 print(node + " fully connected.")
+    #             else:
+    #                 print("WARNING: " + node + " not fully connected.  Expected nodes not found: "
+    #                       + str(listOfMissingNodes))
 
 
 def addToGraph(testGraph, subjValue, predValue, objValue):
