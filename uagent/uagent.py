32ff161e38df3b32b5df09b1fb2168df9359e2d9
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-10-08 11:17:52 -0400

    Added instruction step

--- uagent/uagent.py
+++ uagent/uagent.py
@@ -6,43 +6,60 @@ from think import (Agent, Audition, Chunk, Language, Memory, Motor, Query,
                    Vision)
 
 
+class InstructionStep:
+
+    def __init__(self, memory, rule):
+        self.memory = memory
+        self.rule = rule
+        self.chunk = Chunk(isa='step', rule=self.rule)
+        self.memory.add(self.chunk)
+        self.utility = 0
+
+    def recall_if_necessary(self):
+        if self.utility <= 0:
+            self.memory.recall(isa='step', rule=self.rule)
+
+
 class UndifferentiatedAgent(Agent):
 
     def __init__(self, env, output=True):
         super().__init__(output=output)
 
-        self.memory = OntologyMemory(self)
+        self.memory = Memory(self)
+        self.ontology_memory = OntologyMemory(self)
+
         self.vision = Vision(self, env.display)
         self.audition = Audition(self, env.speakers)
         self.motor = Motor(self, self.vision, env)
 
-        self.interpreter = Interpreter(self.memory)
+        self.interpreter = Interpreter(self.ontology_memory)
 
         self.language = Language(self)
         self.language.add_interpreter(lambda words:
                                       self.interpreter.interpret_ace(' '.join(words)))
 
         #CONTROLLED VOCABULARY TERMS:
-        #CK 2020-06-03: Rough implementation of controlled vocabulary. 
+        #CK 2020-06-03: Rough implementation of controlled vocabulary.
         #Chose to put definitions here, as multiple functions already relate to actions, and I expect that to get more complicated in the future.
         #First version only includes terms THINK is already set up to use.
 
         #Actions that execute_action() can handle.
-        self.action_list = ['press','click','remember']
+        self.action_list = ['press', 'click', 'remember']
         #'press' and 'remember' aren't in the Ontology.
         # "Action" CV in Ontology: 'click', 'read', 'retrieve', 'search', 'listen-for', 'watch-for'
 
         #Conditions that check_condition() can handle.
-        self.condition_list = ['appearsOn','visible']
+        self.condition_list = ['appearsOn', 'visible']
         #'appearsOn' isn't in the Ontology.
         # "Affordance" CV in the Ontology: 'clickable', 'searchable', 'retrievable', 'findable', 'audible', 'visible'
 
         #Not used at the moment.
-        self.item_role_list = ['target','stimulus','distractor','responseButton','infoButton']
+        self.item_role_list = ['target', 'stimulus',
+                               'distractor', 'responseButton', 'infoButton']
         #"ItemRole" in the Ontology.
 
         #For future implementations (trying to use other labs' instructions)
-        self.agent_synonym_list = ['subject','participant','you']
+        self.agent_synonym_list = ['subject', 'participant', 'you']
 
     # subject(subject), screen(screen), letter(target),
     # hasProperty(present,positive), hasProperty(target,correct),
@@ -64,7 +81,7 @@ class UndifferentiatedAgent(Agent):
                 if action.pred == potential_action:
                     return True
         return False
-    
+
     #CK 2020-06-03: Updated for CV terms. Function output unaltered for 'appearsOn' (the only thing it looked for, previously). Treats 'visible' as identical to 'appearsOn' at the moment.
     def check_condition(self, cond, context):
         for potential_condition in self.condition_list:
@@ -85,7 +102,8 @@ class UndifferentiatedAgent(Agent):
                 if cond.pred == 'appearsOn':
                     self.think('check condition "{}"'.format(cond))
                     isa = cond.obj(0)
-                    visual = self.vision.find(isa=isa, seen=False)
+                    # visual = self.vision.find(isa=isa, seen=False)
+                    visual = self.vision.wait_for(isa=isa, seen=False)
                     if visual:
                         print('----- Found {}'.format(isa))
                         context.set('visual', visual)
@@ -133,7 +151,19 @@ class UndifferentiatedAgent(Agent):
         instructions = self.vision.encode(instr_visual)
         self.language.interpret(instructions)
 
+        # while self.time() < time:
+        #     context = Chunk()
+        #     for rule in self.ontology_memory.recall_ground_rules():
+        #         self.process(rule, context)
+
+        steps = []
+        for rule in self.ontology_memory.recall_ground_rules():
+            step = InstructionStep(self.memory, rule)
+            steps.append(step)
+            print(rule)
+
         while self.time() < time:
             context = Chunk()
-            for rule in self.memory.recall_ground_rules():
-                self.process(rule, context)
+            for step in steps:
+                step.recall_if_necessary()
+                self.process(step.rule, context)
