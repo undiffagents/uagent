9ae0c0735aa28eeb69f1d1787679ea878061a42b
Colin Kupitz <kupitzc@gmail.com>
2020-10-09 12:52:59 -0400

    Add comments to new UA functions, prune old comments

--- uagent/uagent.py
+++ uagent/uagent.py
@@ -29,6 +29,7 @@ class InstructionStep:
         self.last_time = None
         self.last_time_was_recall = None
 
+    #Use basic utility function to determine if an instruction step should be recalled from memory then executed, or simply executed
     def execute(self, context):
         self.last_time = self.agent.time()
         c_recall = math.exp(self.utility_recall / math.sqrt(2 * DEFAULT_EGS))
@@ -44,6 +45,7 @@ class InstructionStep:
             self.process(self.rule, context)
             self.last_time_was_recall = False
 
+    #update utility values with time-based reward
     def give_reward(self, time, reward):
         if self.last_time is not None:
             reward -= (time - self.last_time)
@@ -84,16 +86,13 @@ class UndifferentiatedAgent(Agent):
                                       self.interpreter.interpret_ace(' '.join(words)))
 
         #CONTROLLED VOCABULARY TERMS:
-        #CK 2020-06-03: Rough implementation of controlled vocabulary.
-        #Chose to put definitions here, as multiple functions already relate to actions, and I expect that to get more complicated in the future.
-        #First version only includes terms THINK is already set up to use.
 
-        #Actions that execute_action() can handle.
+        #ACTIONS that execute_action() can handle.
         self.action_list = ['press', 'click', 'remember']
         #'press' and 'remember' aren't in the Ontology.
         # "Action" CV in Ontology: 'click', 'read', 'retrieve', 'search', 'listen-for', 'watch-for'
 
-        #Conditions that check_condition() can handle.
+        #CONDITIONS that check_condition() can handle.
         self.condition_list = ['appearsOn', 'visible']
         #'appearsOn' isn't in the Ontology.
         # "Affordance" CV in the Ontology: 'clickable', 'searchable', 'retrievable', 'findable', 'audible', 'visible'
@@ -103,23 +102,9 @@ class UndifferentiatedAgent(Agent):
                                'distractor', 'responseButton', 'infoButton']
         #"ItemRole" in the Ontology.
 
-        #For future implementations (trying to use other labs' instructions)
+        #AGENT NAMES
         self.agent_synonym_list = ['subject', 'participant', 'you']
 
-    # subject(subject), screen(screen), letter(target),
-    # hasProperty(present,positive), hasProperty(target,correct),
-    # appearsOn(target,screen), button(present)
-    # =>
-    # press(subject,present)
-
-    # button(absent), hasProperty(absent,negative),
-    # letter(distractor), screen(screen),
-    # appearsOn(distractor,screen), letter(distractor),
-    # hasProperty(distractor,incorrect), subject(subject)
-    # =>
-    # press(subject,absent)
-
-    #CK 2020-06-03: Updated for CV terms. Function output unaltered for 'press' (the only action it looked for, previously)
     def is_action(self, rule):
         for action in rule.actions:
             for potential_action in self.action_list:
@@ -127,23 +112,10 @@ class UndifferentiatedAgent(Agent):
                     return True
         return False
 
-    #CK 2020-06-03: Updated for CV terms. Function output unaltered for 'appearsOn' (the only thing it looked for, previously). Treats 'visible' as identical to 'appearsOn' at the moment.
     def check_condition(self, cond, context):
         for potential_condition in self.condition_list:
             #as more conditions are added, their cases must be coded in here.
             if cond.pred == potential_condition:
-                # if cond.pred == 'appearsOn' or cond.pred == 'visible':
-                #     self.think('check condition "{}"'.format(cond))
-                #     isa = cond.obj(0)
-                #     # visual = self.vision.find(isa=isa)
-                #     visual = self.vision.search_for(Query(isa=isa), None)
-                #     if visual:
-                #         print('----- Found visual')
-                #         context.set('visual', visual)
-                #         visobj = self.vision.encode(visual)
-                #         context.set(isa, visobj)
-                #     else:
-                #         return False
                 if cond.pred == 'appearsOn':
                     self.think('check condition "{}"'.format(cond))
                     isa = cond.obj(0)
@@ -158,7 +130,6 @@ class UndifferentiatedAgent(Agent):
                         return False
         return True
 
-    #CK 2020-06-03: Updated for CV terms. Function output unaltered for action subject 'subject' (the only identifier it looked for, previously), and for actions 'press', 'click', and 'remember'.
     def execute_action(self, action, context):
         for potential_syn in self.agent_synonym_list:
             if action.obj(0) == potential_syn:
@@ -180,6 +151,9 @@ class UndifferentiatedAgent(Agent):
                 elif action.pred == 'remember':
                     pass
 
+    #1) check if the rule is an action relevant to the UA
+    #2) if so, check if all conditions are met
+    #3) if so, execute the action
     def process(self, rule, context):
         if self.is_action(rule):
             self.think('process rule "{}"'.format(rule))
@@ -207,6 +181,11 @@ class UndifferentiatedAgent(Agent):
 
         self.motor.keyboard.add_type_fn(lambda key: give_reward())
 
+        #Repeatedly loops through the instruction steps.
+        # execute()
+            # uses utility functions to determine if the rule is first recalled from memory, or simply executed.
+            # In either case, the rule is then passed to process() and procedes as in the previous version.
+            # CK->DARIO: I think it'd be a good idea to change the name of this (execute()) function to 'determine_action', 'utility_action' or something similar -- seems more in line with what the function is doing.
         while self.time() < time:
             context = Chunk()
             for step in steps:
