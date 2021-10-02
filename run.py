7fa8ebfe6a8fa96042679b155eda00bf5675f84d
kupitzc <kupitzc@gmail.com>
2021-10-01 21:03:42 -0400

    Overhaul for targetted gap-testing with detailed logging

--- run.py
+++ run.py
@@ -15,10 +15,21 @@ def load_text(path):
 
 if __name__ == '__main__':
 
+
+    do_gaptest = 0 #1 for gap-testing, 0 for normal run
+
+    # gap_name = 'pvtgap_original'
+    # gap_name = 'pvtgap_lexical1' #rare lexical variant (replace 'letter' with 'stimulus')
+    # gap_name = 'pvtgap_lexical2' #non-word (replace 'letter' with 'slook')
+    # gap_name = 'pvtgap_context1' #remove 'screen' (would be inferred by most)
+    # gap_name = 'pvtgap_context2' #remove specification of 'press space_bar to respond'
+    # gap_name = 'pvtgap_context3' #remove 'screen' (would be inferred by most)
+    # curInst = ''.join(['tasks/pvt/gaptests/', gap_name, '.txt'])
+
     #script control flags
     do_outlog = 1 # 0 prints to console, 1 records to /data/logs
-    simulateRules = 0 #1 for testing, 0 for use interpreter
-    stopOldServer = 0 #1 to re-initialize Ontology every run
+    simulateRules = 0 #1 for testing, 0 for use interpreter (not in use yet)
+    stopOldServer = 1 #1 to re-initialize Ontology every run
 
     # Default Settings
     # runtime = 300
@@ -48,6 +59,11 @@ if __name__ == '__main__':
         elif args[0] == '--log' and len(args) > 1:
             do_outlog = args[1]
             args = args[2:]
+        elif args[0] == '--gaptest' and len(args) > 1:
+            do_gaptest = 1
+            gap_name = args[1]
+            curInst = ''.join(['tasks/pvt/gaptests/', gap_name, '.txt'])
+            args = args[2:]
         else:
             print('Unknown arguments: {}'.format(args))
             print(
@@ -66,12 +82,10 @@ if __name__ == '__main__':
     # create environment
     env = Environment(window=window)
 
-    #'tasks/pvt/ace.txt'
-    # curInst = 'tasks/pvt/ace.txt'
-    # typoInst = 'tasks/pvt/gaptests/typo.txt'
-    curInst = 'tasks/pvt/gaptests/pacebar.txt'
     # create task
     if task_name == 'pvt':
+        if not curInst:
+            curInst = '/tasks/pvt/ace.txt' #original instruction path for non-gap testing
         task = (PVTTask(env, load_text(curInst))
                 if agent_name == 'uagent' else
                 PVTTask(env))
@@ -85,8 +99,12 @@ if __name__ == '__main__':
 
     # 0 = console output, 1 = record log (saves to /data/logs/)
     if do_outlog:
+        if do_gaptest:
+            output_name = gap_name
+        else:
+            output_name = task_name
         output = get_think_logger(name='think',
-            logfilename=''.join(['data/logs/', task_name, "_", datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), '.txt']), uselogfile=True)
+            logfilename=''.join(['data/logs/', output_name, "_", datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), '.txt']), uselogfile=True)
     else:
         output = True
 
@@ -117,3 +135,7 @@ if __name__ == '__main__':
     world.run(runtime)
 
     print(''.join(['\nSIMULATION COMPLETE.\n']),end='\n')
+
+    if do_gaptest:
+        os.rename("data/logs/interpreter-logfile.txt", "".join(["data/logs/", gap_name, "-interpreter-logfile.txt"]))
+        os.rename("data/logs/console-logfile.txt", "".join(["data/logs/", gap_name, "-console-logfile.txt"]))
