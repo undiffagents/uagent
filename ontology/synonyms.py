d42610a0f3d36c35f94e9f5ce7b952696b19f6e5
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2021-10-15 14:17:45 -0400

    Improved open() call

--- ontology/synonyms.py
+++ ontology/synonyms.py
@@ -2,7 +2,7 @@ import csv
 
 
 def read_synonyms():
-    with open('ontology/synonyms.csv', mode='r') as f:
+    with open('ontology/synonyms.csv', 'r') as f:
         for line in csv.reader(f):
             if line:
                 yield (line[0], line[1])
